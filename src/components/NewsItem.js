03898aee12a6628915e4418da91bc04a628e6977
Malhar Soomro <77386938+Malhar-Soomro@users.noreply.github.com>
2021-09-07 10:08:51 +0500

    author and published dare added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 export class NewsItem extends Component {
     render() {
-        let { title, description, urlToImage, url, source } = this.props.articles;
+        let { title, description, urlToImage, url, source, author, publishedAt } = this.props.articles;
         return (
             <div className="my-3">
                 <div className="card">
@@ -10,10 +10,10 @@ export class NewsItem extends Component {
                         </span>
                     </div>
                     <img src={urlToImage ? urlToImage : "https://videos.arynews.tv/wp-content/uploads/2019/03/11H6-520x293.jpg"} style={{ height: "190px" }} className="card-img-top" alt="..." />
-                    {source.name}
                     <div className="card-body">
                         <h5 className="card-title">{title ? title.slice(0, 45) : ""}...</h5>
-                        <p className="card-text">{description ? description.slice(0, 88) : ""}...</p>
+                        <p className="card-text">{description ? description.slice(0, 80) : "Call of Duty players have noticed that a Black Ops Cold War cosmetic has been ch.."}...</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(publishedAt).toGMTString()}</small></p>
                         <a href={url} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
